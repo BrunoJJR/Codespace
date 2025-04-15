e03b8539e1e68064d4049df8f7c413067e47c7ce
BrunoJJR <brunojjramos@gmail.com>
2025-04-15 15:21:51 +0100

    Has add expense working with main formatted properly

--- Apps/expense_tracker/main.py
+++ Apps/expense_tracker/main.py
@@ -1,41 +1,45 @@
 import json
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(
+# Defining main functions
+
+def setup_file_paths():
+    BASE_DIR = os.path.dirname(os.path.abspath(
     __file__))  # folder where the app is
-DATA_DIR = os.path.join(BASE_DIR, "data")  # folder where the data directory is
-# folder where 'expenses.json' is
-FILE_PATH = os.path.join(DATA_DIR, "expenses.json")
+    DATA_DIR = os.path.join(BASE_DIR, "data")  # folder where the data directory is
+    # folder where 'expenses.json' is
+    FILE_PATH = os.path.join(DATA_DIR, "expenses.json")
+    # create a 'data' directory if there isn't one yet
+    os.makedirs(DATA_DIR, exist_ok=True)
+    return FILE_PATH
 
-# create a 'data' directory if there isn't one yet
-os.makedirs(DATA_DIR, exist_ok=True)
+def load_expenses(FILE_PATH):
+    if os.path.exists(FILE_PATH):
+        try:    # try reading as JSON but if decode error start with empty list instead
+            with open(FILE_PATH, "r") as file:
+                return json.load(file)
+        except json.JSONDecodeError:
+            return []
+    else:
+        return []
 
-if os.path.exists(FILE_PATH):
-    try:    # try reading as JSON but if decode error start with empty list instead
-        with open(FILE_PATH, "r") as file:
-            expenses = json.load(file)
-    except json.JSONDecodeError:
-        expenses = []
-else:
-    expenses = []
+def save_expenses(expenses, FILE_PATH):
+    with open(FILE_PATH, "w") as file:
+        json.dump(expenses, file, indent=2)
+        
+def add_expenses(expenses, FILE_PATH):
+    n = int(input("How many expenses do you want to add?\n"))
+    for i in range(n):
+        expense_name = input("Enter the expense: ")
+        expense_cost = float(input("Enter the cost of the expense: "))
+        expenses.append({"Expense": expense_name, "Cost": expense_cost})
+    save_expenses(expenses, FILE_PATH)
+        
+# main variables
+FILE_PATH = setup_file_paths()  
+expenses = load_expenses(FILE_PATH)
 
-total_cost = 0
-
-for i in range(3):
-    expense_name = input("Enter an expense: ")
-    expense_cost = float(input("Enter the cost of the expense: "))
-    expenses.append({"Expense": expense_name, "Cost": expense_cost})
-    total_cost += expense_cost
-
-for expense in expenses:
-    print(f"You have bought {expense['Expense']} for {expense['Cost']}$.")
-
-print(f"\nThat brings us to a grand total of {total_cost}$!")
-
-with open(FILE_PATH, "w") as file:
-    json.dump(expenses, file, indent=2)
-
-
+# Main CLI Menu
 while True:
     print("Options: 1 - Add Expense; 2 - Remove Expense; 3 - Edit Expense; 4 - View Expenses; 5 - Quit")
     user_cmd = input()
@@ -44,15 +48,15 @@ while True:
         break
     
     elif user_cmd in ["1", "add"]:
-        #add_expense()
-        print("Not Implemented Yet")
+        add_expenses(expenses, FILE_PATH)
+        
     elif user_cmd in ["2", "rmv"]:
         #rmv_expense()
         print("Not Implemented Yet")
     elif user_cmd in ["3", "edit"]:
         #edit_expense()
         print("Not Implemented Yet")
-    elif user_cmd in ["4", "rmv"]:
+    elif user_cmd in ["4", "view"]:
         #view_expense()
         print("Not Implemented Yet")
         
